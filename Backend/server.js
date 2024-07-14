36ee602d79364183bba4a6b3348b89080e139b79
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-07-14 13:52:07 +0530

    update
    update

--- Backend/server.js
+++ Backend/server.js
@@ -8,8 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());   //parse req.body
+
 
 app.use("/api/auth",authRoutes);
+app.use(express.urlencoded({ extended: true })); 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
